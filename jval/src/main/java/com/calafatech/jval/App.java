66f6bde48cadf6fe83ddbd8eec6dc43dd627c474
Uziel Perez <uvperez@hotmail.com>
2024-03-08 21:21:25 -0300

    project creation

---
+++ jval/src/main/java/com/calafatech/jval/App.java
@@ -0,0 +1,13 @@
+package com.calafatech.jval;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
